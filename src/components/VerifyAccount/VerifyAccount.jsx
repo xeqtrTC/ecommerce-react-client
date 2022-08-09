43455fddaf7c801a2d1918bf7b03c07a2b1c2020
xeqtrTC <anunnakifoxtgtb2@gmail.com>
2022-08-09 20:04:26 +0200

    fixes

--- src/components/VerifyAccount/VerifyAccount.jsx
+++ src/components/VerifyAccount/VerifyAccount.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 import './VerifyAccount.css';
 import { emailVerification } from '../../actions/userActions';
@@ -14,9 +14,15 @@ import { useSelector, useDispatch } from 'react-redux';
 export default function VerifyAccount() {
     const {token} = useParams();
     console.log(token);
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const {isLoading, success, isError} = useSelector((state) => state.emailVerificationUser);
     console.log(isLoading, success, isLoading);
+    useEffect(() => {
+        if(success) {
+            navigate('/login')
+        }
+    }, [success])
     useEffect(() => {
         dispatch(emailVerification(token));
     }, [dispatch, emailVerification])
